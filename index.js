f0ab64e742c3e6b2736134dbbd071bb5df4619ce
Subhankar Maiti <subhankarmaiti40@gmail.com>
2020-03-10 01:09:41 +0530

    basic implementation

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 const io = require("socket.io")();
+const uuidv1 = require("uuid/v1");
 const messageHandler = require("./handlers/message.handler");
 
-let userIds = {};
-let currentUserId = 2;
+let users = {};
 
 function createUserAvatarUrl() {
   const rand1 = Math.round(Math.random() * 200 + 100);
   const rand2 = Math.round(Math.random() * 200 + 100);
   return `https://placeimg.com/${rand1}/${rand2}/any`;
 }
-
+function createUsersOnline() {
+  const values = Object.values(users);
+  const onlyWithUsername = values.filter(u => u.username !== undefined);
+  return onlyWithUsername;
+}
 io.on("connection", socket => {
   console.log("a user connected!");
-  userIds[socket.id] = { userId: currentUserId++ };
-  socket.on("join", username => {
-    userIds[socket.id].username = username;
-    userIds[socket.id].avatar = createUserAvatarUrl();
-    messageHandler.handleMessage(socket, userIds);
+  users[socket.id] = { userId: uuidv1() };
+
+  socket.on("disconnect", () => {
+    delete users[socket.id];
+    io.emit("action", { type: "users_online", data: createUsersOnline() });
   });
   socket.on("action", action => {
     switch (action.type) {
-      case "server/hello":
-        console.log("Got hello action", action.payload);
-        socket.emit("action", { type: "message", data: "Good day!" });
-        break;
       case "server/join":
         console.log("Git join event", action.payload);
-        userIds[socket.id].username = action.payload;
-        userIds[socket.id].avatar = createUserAvatarUrl();
+        users[socket.id].username = action.payload;
+        users[socket.id].avatar = createUserAvatarUrl();
+
+        io.emit("action", { type: "users_online", data: createUsersOnline() });
+        socket.emit("action", { type: "self_user", data: users[socket.id] });
+        break;
+      case "server/private_message":
+        const conversationId = action.data.conversationId;
+        const from = users[socket.id].userId;
+        const userValues = Object.values(users);
+        const socketIds = Object.keys(users);
+        for (let i = 0; i < userValues.length; i++) {
+          if (userValues[i].userId === conversationId) {
+            const socketId = socketIds[i];
+            io.sockets.sockets[socketId].emit("action", {
+              type: "private_message",
+              data: {
+                ...action.data,
+                conversationId: from
+              }
+            });
+          }
+        }
         break;
     }
   });
